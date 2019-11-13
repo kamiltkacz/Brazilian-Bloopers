89cb835d06252567a01ebc45f99a72038a5de926
Kamil Tkacz <50182884+kamiltkacz@users.noreply.github.com>
2019-11-13 09:44:45 -0300

    Removed more bootstrap

--- index.php
+++ index.php
@@ -247,9 +247,7 @@ require_once('./dbconn.php');
       // Get the element with id="defaultOpen" and click on it
             document.getElementById("defaultOpen").click();
     </script>   
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-      </script>      
+    
      
   </body>
 </html>         
