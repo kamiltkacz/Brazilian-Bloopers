3005f04bbc0fd7dc330d4f2f3d2a874b9f05f642
Kamil Tkacz <50182884+kamiltkacz@users.noreply.github.com>
2020-04-30 17:47:01 -0300

    closing tag added

--- quiz.php
+++ quiz.php
@@ -3,34 +3,34 @@
 
 require_once('./dbconn.php');
 
- $sql = "SELECT * FROM questions";
-      $result=mysqli_query($conn,$sql) or die(mysqli_error($conn));
-      $num = mysqli_num_rows($result);
+$sql = "SELECT * FROM questions";
+$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+$num = mysqli_num_rows($result);
 
-       while ($r=mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+while ($r = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
 
-$sql2 ="SELECT * FROM offeredAnswer WHERE questionId = '".$r['id']."'";
-$result2 = mysqli_query($conn,$sql2) or die(mysqli_error($conn));
-
-
-$target = $r['targetWord'];
+    $sql2 = "SELECT * FROM offeredAnswer WHERE questionId = '" . $r['id'] . "'";
+    $result2 = mysqli_query($conn, $sql2) or die(mysqli_error($conn));
 
 
-$ex = explode($target,$r['question']);
-
-echo '<br>'.$ex[0];
-
-echo '<select>';
-
-while ($r2=mysqli_fetch_array($result2,MYSQLI_ASSOC)) {
+    $target = $r['targetWord'];
 
 
-    echo '<option>'.$r2['offeredAnswer'].'</option>';
+    $ex = explode($target, $r['question']);
 
+    echo '<br>' . $ex[0];
+
+    echo '<select>';
+
+    while ($r2 = mysqli_fetch_array($result2, MYSQLI_ASSOC)) {
+
+
+        echo '<option>' . $r2['offeredAnswer'] . '</option>';
+    }
+
+    echo '</select>';
+
+    echo $ex[1];
 }
 
-echo '</select>';
-
-echo $ex[1];
-
-       }
+?>
