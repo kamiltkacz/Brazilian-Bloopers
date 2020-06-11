ce10a7e8971e46ded67e6d350cc8c9f7d6218229
Kamil Tkacz <50182884+kamiltkacz@users.noreply.github.com>
2020-06-11 17:31:12 -0300

    in first on.click!! Works

--- index.php
+++ index.php
@@ -486,7 +486,7 @@ require_once('./dbconn.php');
 
 
 
-  //  $(document).ready(function() {
+   $(document).ready(function() {
 
       $("#start_quiz").on("click", function() {
 
@@ -551,7 +551,7 @@ require_once('./dbconn.php');
           <p id="instr" name="instructions">Instructions</p>
 
           <button id='backHome-1'><a href='https://www.brazilianbloopers.com' style='color: black'>Home</a></button>
-          <button id="check_first" type="submit" name="checkfirst" onSubmit="return false">Check</button>
+          <button id="check_first" type="submit" name="checkfirst">Check</button>
 
 
            </form>
@@ -575,10 +575,51 @@ require_once('./dbconn.php');
       });
 
     });
+    // Broken From here
+$("#quiz_form").submit(function() {
+    return false;
+    console.log('return false');
+    });
+
+    $("#check_first").on("click", function() {
+      var formData = $("#quiz_form :input").serializeArray();
+      formData[formData.length] = {
+        name: "action",
+        value: "check_first"
+      };
+      formData.push({});
+      console.log('dataQuiz');
+
+      $.ajax({
+        type: "POST",
+        url: $("#quiz_form").attr("action"),
+        data: formData,
+        dataType: 'json',
+        async: true,
+
+        success: function(result) {
+
+
+          if (result.result == 1) {
+
+
+            alert('yes');
+          }
+
+          if (result.result == 2) {
+            alert('no way');
+          }
+
+        } //ajax, success function 2//
+      }); //ajax 2//
+    }); //submit on.click function 2//
+
 
   });
 
-//});
+});
+
+
 
 
   </script>
@@ -756,7 +797,7 @@ require_once('./dbconn.php');
 
   </script>
 
-<script src="first_page.js"></script>
+<!--<script src="first_page.js"></script>-->
 
 
 </body>
