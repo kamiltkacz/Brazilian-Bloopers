5f8519d39c0272a216376c5ea0fbad0e6cd26500
Kamil Tkacz <50182884+kamiltkacz@users.noreply.github.com>
2020-08-05 18:14:18 -0300

    sub_quiz btn

--- actions.php
+++ actions.php
@@ -127,21 +127,12 @@ if (isset($_POST["action"])) {
 }
 
 
-if ($action == "sub") {
+if ($action == "sub_quiz") {
     $export = Array();
     $news = $_POST["news"];
     $weather = $_POST["weather"];
     $dist = $_POST["dist"];
 
-    // $export['newsErr'] = "";
-
-    // if (empty($news)) {
-
-    //   $export['newsErr'] = "empty";
-    //   $export['news'] = 0;
-    //   } else {
-    //   $export['news'] = 1;
-    //  }
 
     $sql = "INSERT INTO quiz (Radio_1, Radio_2, Radio_3)
     VALUES ('$news', '$weather', '$dist')";
