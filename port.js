075dbad0c7898559525462fe2bdf93e8a44dbbbc
Kamil Tkacz <50182884+kamiltkacz@users.noreply.github.com>
2020-11-16 20:09:21 -0300

    accesibility port

--- port.js
+++ port.js
@@ -20,9 +20,9 @@ const tabsSwitch = $(`<div class="tabs">
     <a href="#" class="active">Contato</a>
   </button>
 
-  <img src="//bandidosnatv.com/plgns/gtranslate/flags/24/en-us.png" id="eng_btn" height="24" width="24" alt="English">
+  <img src="//bandidosnatv.com/plgns/gtranslate/flags/24/en-us.png" id="eng_btn" height="24" width="24" alt="English_flag">
   <div id="port_icon>
-  <img src="//bandidosnatv.com/plgns/gtranslate/flags/24/pt-br.png" id="port_btn" height="24" width="24" alt="Portuguese">
+  <img src="//bandidosnatv.com/plgns/gtranslate/flags/24/pt-br.png" id="port_btn" height="24" width="24" alt="Portuguese_flag">
  </div>
 </div>`);
 
@@ -37,66 +37,66 @@ const headerSwitch = $(`
 const aboutSwitch = $(` <div id="About" class="tabcontent">
   <div class="container" id="about_us">
   <fieldset class="field_about">
-     <legend class="leg_highlight" id="leg_high_1">Sobre nós</legend>
+     <h4 class="leg_highlight" id="leg_high_1">Sobre nós</h4>
       <p class="al_left">Somos um grupo de professores de inglês baseados em Brasília, Brasil, que estão determinados a aprimorar suas habilidades. Compilamos este quiz com base em anos de experiência. <br> Um dia, fascinados por vários "brasilismos" que os alunos ficavam atirando em nossos ouvidos, decidimos que esse fenômeno merecia um site próprio. Assim, o projeto Brazilian Bloopers ganhou vida.</p>
   </fieldset>
   </div>
     <div class="container">
     <fieldset class="field_about">
-      <legend class="leg_highlight" id="leg_high_2" >Sobre os Bloopers</legend>
-      <p class="al_left">Vamos encarar. Aprender outro idioma e comunicar suas ideias nele não é tarefa fácil. O inglês, embora onipresente neste mundo, não é exceção. Estamos aqui para ajudar um pouco nisso.<br>Se você procurar a definição em inglês da palavra <a href="https://www.dictionary.com/browse/blooper" style="background: radial-gradient(#2c00f7, #00000059); border-radius: 20px; padding: 5px" target="_blank"><span>'blooper'</span></a>&nbsp;você receberá "um erro embaraçoso que você comete publicamente". Embora não achemos que os erros que você encontrará aqui sejam tão graves, acreditamos que corrigi-los é um grande passo em sua jornada de aprendizado de idiomas.<br><a href="#examples" id="a_examples">&nbsp;<span>Confira os exemplos abaixo.</span></a></p>
+      <h4 class="leg_highlight" id="leg_high_2" >Sobre os Bloopers</h4>
+      <p class="al_left">Vamos encarar. Aprender outro idioma e comunicar suas ideias nele não é tarefa fácil. O inglês, embora onipresente neste mundo, não é exceção. Estamos aqui para ajudar um pouco nisso.<br>Se você procurar a definição em inglês da palavra <a href="https://www.dictionary.com/browse/blooper" alt="to dictionary.com" style="background: radial-gradient(#ffc107, #00000059); border-radius: 20px; padding: 5px" target="_blank"><span>'blooper'</span></a>&nbsp;você receberá "um erro embaraçoso que você comete publicamente". Embora não achemos que os erros que você encontrará aqui sejam tão graves, acreditamos que corrigi-los é um grande passo em sua jornada de aprendizado de idiomas.<br><a href="#examples" id="a_examples" style="background: radial-gradient(#ffc107, #00000059);border-radius: 20px; padding: 5px">&nbsp;<span>Confira os exemplos abaixo.</span></a></p>
     </fieldset>
     </div>
 
   <div class="container" id="about_qz">
   <fieldset class="field_about">
-     <legend class="leg_highlight" id="leg_high_3">Sobre o Quiz</legend>
+     <h4 class="leg_highlight" id="leg_high_3">Sobre o Quiz</h4>
      <p class="al_left"> O teste é apenas em inglês, porque não pode ficar muito fácil, não é? A pesquisa, entretanto, é oferecida em inglês e português.
-     O quiz consiste em falsos cognatos, expressões idiomáticas, frases incompletas, etc. Você verá uma série de diálogos. Você deve usar seu bom senso ao escolher a única resposta correta.<br><br>&nbsp;Existem 30 questões que variam em dificuldade. Alguns erros são mais comuns e outros menos frequentes, mas todos eles compartilham a peculiaridade da língua portuguesa que não traduz bem para o inglês. Mesmo que muitos dos erros sejam inteligíveis para falantes nativos de inglês, seria melhor você encontrar uma alternativa mais comum.<br><br>&nbsp;Mas não seja tão duro consigo mesmo - Tudo bem se você tem um pedaço do Brasil que nunca sai do seu cérebro, e por que não deixar o gringo pensar um pouco?<br><br>Não se esqueça de aproveitar o processo!<br><br><a href="#" style="background: radial-gradient(#2c00f7, #00000059); border-radius: 20px; padding: 5px", onclick="openPage('TheQuiz', this);">Pronto?</a></p>
+     O quiz consiste em falsos cognatos, expressões idiomáticas, frases incompletas, etc. Você verá uma série de diálogos. Você deve usar seu bom senso ao escolher a única resposta correta.<br><br>&nbsp;Existem 30 questões que variam em dificuldade. Alguns erros são mais comuns e outros menos frequentes, mas todos eles compartilham a peculiaridade da língua portuguesa que não traduz bem para o inglês. Mesmo que muitos dos erros sejam inteligíveis para falantes nativos de inglês, seria melhor você encontrar uma alternativa mais comum.<br><br>&nbsp;Mas não seja tão duro consigo mesmo - Tudo bem se você tem um pedaço do Brasil que nunca sai do seu cérebro, e por que não deixar o gringo pensar um pouco?<br><br>Não se esqueça de aproveitar o processo!<br><br><a href="#" style="background: radial-gradient(#ffc107, #00000059); border-radius: 20px; padding: 5px", onclick="openPage('TheQuiz', this);">Pronto?</a></p>
    </fieldset>
    </div>
    <div class="container">
       <picture>
-        <legend class="leg_highlight"><a id="examples" style="font-size: 25px;">Exemplo de um erro brasileiro em um diálogo casual:</a></legend>
+        <h4 class="leg_highlight"><a id="examples" style="font-size: 25px;">Exemplo de um erro brasileiro em um diálogo casual:</a></h4>
        <br><br><p>A) Did you like your trip?</p><br><p style="margin-top: -20px;">B)&nbsp;<span class="error_style"><em>More or less.</em></span></p><br><p style="margin-top: -20px;">A) Hmmm...&#129300;</p>
        <hr>
        <p class="al_left">&nbsp;<span class="span_exp">Explanation:</span>&nbsp;In English we generally say "More or Less" to use it as an approximation of numbers, quantities, a "plus-minus". Ex. "It will cost you 40 reais, more or less."<br><br>We don't answer with "more or less" only to express value or our impression of something. If you thought the above mentioned trip could have been better, you answer "Not much". If you thought it could have been worse, you answer "It was okay." You should remember that answers like this are hugely context dependent, meaning, you will have to develop what you mean in more detail.</p>
       </picture>
     </div>
     <div class="container">
-      <legend class="leg_highlight">Exemplo nº 1 de Bloopers no mainstream.</legend><br>
+      <h4 class="leg_highlight">Exemplo nº 1 de Bloopers no mainstream.</h4><br>
       <p class="al_left">O hit de Michel Telo, uma canção brasileira muito popular, tem uma leitura um pouco áspera em inglês. Entre outros erros menores, o título "Se eu te pegar" dá como algo traduzido do português de maneira desajeitada. Mesmo o showbusiness mainstream não está livre de erros ocasionais.</p>
       <div class="yt-resp">
-        <iframe class="iframe-resp" src="https://www.youtube.com/embed/CwC5BFX7rqQ">
+        <iframe class="iframe-resp" src="https://www.youtube.com/embed/CwC5BFX7rqQ" alt="youtube_link_1">
         </iframe>
       </div>
       <p class="al_left">Abaixo está uma versão muito melhor, onde o cantor americano Pitbull dá seu próprio toque às letras. "Se eu te pegar" torna-se "Se eu te pegar" e "assim você vai me matar" se traduz em "você está jogando duro para conseguir". Bom trabalho Pitbull</p>
       <div class="yt-resp">
-        <iframe class="iframe-resp" src="https://www.youtube.com/embed/bMMnn2kA_HY">
+        <iframe class="iframe-resp" src="https://www.youtube.com/embed/bMMnn2kA_HY" alt="youtube_link_2">
         </iframe>
         </div>
     </div>
     <div class="container">
-    <legend class="leg_highlight">Exemplo nº 2 de Bloopers no mainstream.</legend><br>
+    <h4 class="leg_highlight">Exemplo nº 2 de Bloopers no mainstream.</h4><br>
       <p class="al_left">A entrevista de Marilia Gabriela com a única Madonna é claramente estranha, em parte devido à incapacidade do jornalista brasileiro de colocar seu inglês nos trilhos. Madonna claramente não era caridosa e estava com vontade de ajudar.</p>
         <div class="yt-resp">
-          <iframe class="iframe-resp" src="https://www.youtube.com/embed/zHMhLd4MUC4">
+          <iframe class="iframe-resp" src="https://www.youtube.com/embed/zHMhLd4MUC4" alt="youtube_link_3">
           </iframe>
         </div>
     </div>
 
 </div>`);
 
-const legStepsSwitch = $(
-  `<legend id="leg_steps">Siga estas 3 etapas simples:</legend>`
+let legStepsSwitch = $(
+  `<h4 id="leg_steps">Siga estas 3 etapas simples:</h4>`
 );
-const clickSurveySwitch = $(
-  `<p id="click_survey">1. Primeiro, preencha nossa pesquisa espetacularmente curta.</p>`
+let clickSurveySwitch = $(
+  `<p id="click_survey">1.&nbsp;Primeiro, preencha nossa pesquisa espetacularmente curta.</p>`
 );
-const clickStartSwitch = $(
-  `<p id="click_start">2. Em seguida, clique no botão "Iniciar" para iniciar o questionário.</p><br>`
+let clickStartSwitch = $(
+  `<p id="click_start">2.&nbsp;Em seguida, clique no botão "Iniciar" para iniciar o quiz.</p>`
 );
-const haveFunSwitch = $(`<p id="have_fun">3.Divirta-se!</p>`);
+let haveFunSwitch = $(`<p id="have_fun">3.&nbsp;Divirta-se!</p>`);
 
 const feedbackSwitch = $(` <div id="feedback_div" style="text-align:center">
       <h3 id="shoot_email">Se você gostou do quiz diga oi para mim no<br><i class="fab fa-twitter"></i><a href="https://twitter.com/kamiltkacz" style="font-size: unset;">&nbsp;@kamiltkacz</a><br>manda um e-mail diretamente </h3>
@@ -106,13 +106,13 @@ const feedbackSwitch = $(` <div id="feedback_div" style="text-align:center">
 
 const contactSwitch = $(` <div id="contact_text">
             <label for="name">Nome*<span id="span_name"></span></label>
-          <input type="text" id="bor_name" name="name" placeholder="Seu nome.." font-family="cursive">
+          <input type="text" id="bor_name" name="name" placeholder="Seu nome.." tabindex="0" font-family="cursive">
             <label for="email">E-mail*<span id="span_email"></span></label>
-          <input type="text" id="bor_email" name="email" placeholder="Seu e-mail..">
+          <input type="text" id="bor_email" name="email" placeholder="Seu e-mail.." tabindex="0">
             <label for="subject">Assunto</label>
-          <input type="text" id="bor_subject" name="subject" placeholder="Sugestão, comentário, etc.">
+          <input type="text" id="bor_subject" name="subject" placeholder="Sugestão, comentário, etc." tabindex="0">
             <label for="message">Mensagem*<span id="span_message"></span></label>
-          <textarea id="bor_message" name="message" placeholder="Escreva sua mensagem aqui!" style="height:170px"></textarea>
+          <textarea id="bor_message" name="message" placeholder="Escreva sua mensagem aqui!" style="height:170px" tabindex="0"></textarea>
           </div>`);
 
 const footerSwitch = $(`<div id="footer" class="container">
@@ -151,7 +151,7 @@ const ques_7_Switch = $(
 
 $("#port_btn").on("click", function () {
 
-  isClicked = true;
+
 
 
   $(".tabs").replaceWith(tabsSwitch);
