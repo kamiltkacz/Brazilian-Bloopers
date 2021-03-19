1ed095b4e1c6d0d95eca1e55d3ecec23e4dcd2bb
Kamil Tkacz <kamiltkacz@gmail.com>
2021-03-19 20:50:49 -0300

    incluir_jquery()

--- subdomain/wp-content/themes/great-idea-theme/functions.php
+++ subdomain/wp-content/themes/great-idea-theme/functions.php
@@ -7,10 +7,37 @@ wp_register_style('stylesheet', get_template_directory_uri() . "/css/boootstrap/
 
 wp_enqueue_style('stylesheet');
 
-wp_register_style('mystyle', get_template_directory_uri() . "/css/boootstrap/min.css", array(), false, 'all');
+wp_register_style('mystyle', get_template_directory_uri() . "/style.css", array(), false, 'all');
 
 wp_enqueue_style('mystyle');
 
 
 }
 add_action('wp_enqueue_scripts', 'carrega_stylesheets');
+
+function incluir_jquery() {
+
+
+wp_deregister_script('jquery');
+
+wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery-3.6.0.min.js', '', 1, true);
+
+add_action('wp_enqueue_scripts', 'jquery');
+
+}
+
+add_action('wp_enqueue_scripts', 'incluir_jquery');
+
+
+
+
+
+function carrega_js() {
+
+
+wp_register_script('myscripts', get_template_directory_uri() . '/js/scripts.js', '', 1, true);
+wp_enqueue_script('myscripts');
+
+
+}
+add_action('wp_enqueue_scripts', 'carrega_js');
