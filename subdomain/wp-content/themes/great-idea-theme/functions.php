4cc47373d682f6d3ff0ec3a6b2186711d513f42f
Kamil Tkacz <kamiltkacz@gmail.com>
2021-03-19 19:22:10 -0300

    ++ mystyle

--- subdomain/wp-content/themes/great-idea-theme/functions.php
+++ subdomain/wp-content/themes/great-idea-theme/functions.php
@@ -7,6 +7,10 @@ wp_register_style('stylesheet', get_template_directory_uri() . "/css/boootstrap/
 
 wp_enqueue_style('stylesheet');
 
+wp_register_style('mystyle', get_template_directory_uri() . "/css/boootstrap/min.css", array(), false, 'all');
+
+wp_enqueue_style('mystyle');
+
 
 }
 add_action('wp_enqueue_scripts', 'carrega_stylesheets');
