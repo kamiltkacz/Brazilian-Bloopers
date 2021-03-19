610b5d97e95d8ba2671b0646d3b2433b0fcb5935
Kamil Tkacz <kamiltkacz@gmail.com>
2021-03-19 17:24:55 -0300

    carregaStylesheets (bootstrap)

---
+++ subdomain/wp-content/themes/Great-Idea Theme/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function carregaStylesheets() {
+
+    wp_register_style('stylesheet', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all' );
+
+    wp_enqueue_style('stylesheet');
+
+
+}
+add_action('wp_enqueue_style()', 'carregaStylesheets()');
