e3e00c427e0ba5587878061a62287e0ae0887ec5
Kamil Tkacz <kamiltkacz@gmail.com>
2021-03-19 22:31:02 -0300

    the_title + have_posts() loop

--- subdomain/wp-content/themes/great-idea-theme/front-page.php
+++ subdomain/wp-content/themes/great-idea-theme/front-page.php
@@ -1,4 +1,14 @@
 <?php get_header();?>
-<h1>Welcome to my website</h1>
+<div class="container">
+
+<h1><?php the_title();?></title></h1>
+
+<?php if (have_posts()) : while(have_posts()) : the_post();?>
+
+<?php the_content();?>
+
+<?php endwhile; endif;?>
+</div>
+
 
 <?php get_footer();?>
