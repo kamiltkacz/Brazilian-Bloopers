ce6b92a5a9247827a885eab2f05b9398026cbaff
Kamil Tkacz <kamiltkacz@gmail.com>
2021-03-19 22:32:41 -0300

    wp_nav_menu() + navigation style

--- subdomain/wp-content/themes/great-idea-theme/header.php
+++ subdomain/wp-content/themes/great-idea-theme/header.php
@@ -1,14 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <!-- <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Document</title> -->
     <?php  wp_head();?>
 </head>
 <body <?php body_class();?>>
 
 <header class="sticky-top">
+
+<div class="container">
+
+<?php wp_nav_menu(
+
+array(
+    'theme_location' => 'top-menu',
+    'menu_class' => 'navigation'
+
+
+)
+);?>
+
+
+</div>
+
+
 
 </header>
