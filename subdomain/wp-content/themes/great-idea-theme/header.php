b430325b838db64f89f97919c832a25277eaa79e
Kamil Tkacz <kamiltkacz@gmail.com>
2021-03-19 23:53:57 -0300

    <meta> + <title>

--- subdomain/wp-content/themes/great-idea-theme/header.php
+++ subdomain/wp-content/themes/great-idea-theme/header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title> -->
+    <title>Otima Idea Wordpress</title>
     <?php  wp_head();?>
 </head>
 <body <?php body_class();?>>
@@ -12,6 +12,8 @@
 <header class="sticky-top">
 
 <div class="container">
+
+
 
 <?php wp_nav_menu(
 
