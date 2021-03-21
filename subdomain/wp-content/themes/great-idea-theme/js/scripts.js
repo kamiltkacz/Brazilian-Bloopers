381e21235366fd442135b73a849be93145683ad0
Kamil Tkacz <kamiltkacz@gmail.com>
2021-03-20 22:36:16 -0300

    span setTimeout

--- subdomain/wp-content/themes/great-idea-theme/js/scripts.js
+++ subdomain/wp-content/themes/great-idea-theme/js/scripts.js
@@ -5,8 +5,33 @@ $(document).ready(function(){
 
 
     let homeheadDiv = $(".homehead");
-    homeheadDiv.animate({top: '200px'}, "slow");
+    homeheadDiv.animate({top: '300px'}, "slow");
     homeheadDiv.animate({fontSize: '3em'}, "slow");
+    homeheadDiv.animate({top: ''}, "slow");
+    homeheadDiv.animate({top: '400px'}, "slow");
+
+ let spanTimeout = $("span");
+     setTimeout(function(){
+
+    $(spanTimeout).css({"line-height": "1"});
+
+    $(window).resize(function() {
+        if ($(window).width() < 480) {
+
+            $(spanTimeout).css({"line-height": "10"});
+        }
+
+    });
+  }, 4000);
+
+  setTimeout(function(){
+
+    $(spanTimeout).animate({left: '200px'}, "slow").slideUp();
+
+
+  }, 15000);
+
+
 
 
 
