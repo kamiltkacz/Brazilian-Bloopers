c9158343ea5bae42f2d779b4b1c42ebf76aeebf0
Kamil Tkacz <kamiltkacz@gmail.com>
2021-03-21 01:31:48 -0300

    span + setTimeout rem

--- subdomain/wp-content/themes/great-idea-theme/js/scripts.js
+++ subdomain/wp-content/themes/great-idea-theme/js/scripts.js
@@ -5,32 +5,22 @@ $(document).ready(function(){
 
 
     let homeheadDiv = $(".homehead");
-    homeheadDiv.animate({top: '300px'}, "slow");
+    homeheadDiv.animate({top: '320px'}, "slow");
     homeheadDiv.animate({fontSize: '3em'}, "slow");
     homeheadDiv.animate({top: ''}, "slow");
-    homeheadDiv.animate({top: '400px'}, "slow");
-
- let spanTimeout = $("span");
-     setTimeout(function(){
-
-    $(spanTimeout).css({"line-height": "1"});
-
-    $(window).resize(function() {
-        if ($(window).width() < 480) {
-
-            $(spanTimeout).css({"line-height": "10"});
-        }
-
-    });
-  }, 4000);
-
-  setTimeout(function(){
-
-    $(spanTimeout).animate({left: '200px'}, "slow").slideUp();
+    homeheadDiv.animate({top: '320px'}, "slow");
 
 
-  }, 15000);
 
+
+
+
+});
+
+$("#arrow-icon").click(function(){
+
+
+alert("hi");
 
 
 
