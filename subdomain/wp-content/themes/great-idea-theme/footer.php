7e0359b810491bb19a7d48540d7bc0fd9559c645
Kamil Tkacz <kamiltkacz@gmail.com>
2021-03-23 14:03:55 -0300

    Accento

--- subdomain/wp-content/themes/great-idea-theme/footer.php
+++ subdomain/wp-content/themes/great-idea-theme/footer.php
@@ -2,7 +2,7 @@
 
 <div class="container">
     <p id="footer" style="margin-top: 550px;
-    margin-left: 550px;">Otima Idea - Test Theme @Kamil Tkacz</p>
+    margin-left: 550px;">Ótima Ideia - Test Theme @Kamil Tkacz</p>
 </div>
 
 </body>
