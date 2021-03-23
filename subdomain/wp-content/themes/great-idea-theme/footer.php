17a52ff26f85a5d630c784845f9e797a1496d4d9
Kamil Tkacz <kamiltkacz@gmail.com>
2021-03-23 01:23:30 -0300

    footer add + style + media

--- subdomain/wp-content/themes/great-idea-theme/footer.php
+++ subdomain/wp-content/themes/great-idea-theme/footer.php
@@ -1,6 +1,9 @@
 <?php wp_footer();?>
 
-
+<div class="container">
+    <p id="footer" style="margin-top: 550px;
+    margin-left: 550px;">Otima Idea - Test Theme @Kamil Tkacz</p>
+</div>
 
 </body>
 </html>
