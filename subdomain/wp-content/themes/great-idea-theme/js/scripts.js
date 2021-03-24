6f6287c48298aaa01ed5d6701b93d622095e2d3d
Kamil Tkacz <kamiltkacz@gmail.com>
2021-03-23 22:28:34 -0300

    3,4,5 columns show()

--- subdomain/wp-content/themes/great-idea-theme/js/scripts.js
+++ subdomain/wp-content/themes/great-idea-theme/js/scripts.js
@@ -32,8 +32,18 @@ $("#arrow-icon-1").click(function(){
     }, "slow");
 
 
-
+    $("#b-sucesso").show("slow");
     $("#col-1").hide();
+    $(document).ready(function() {
+      function myCvc() {
+
+        $("#col-3").show();
+
+      }
+      setTimeout(myCvc, 2000);
+
+    });
+
 
 
  });
@@ -47,11 +57,11 @@ $("#arrow-icon-1").click(function(){
     }, "slow");
 
     let columnThree = $("#col-3");
-    columnThree.animate({
-    height: '90%',
-    width: '55%'
-    }, "slow");
-    columnThree.animate({height: '101%', width: '65%'}, "slow");
+    // columnThree.animate({
+    // height: '90%',
+    // width: '55%'
+    // }, "slow");
+    columnThree.css({marginTop: '0%'});
 
     $("#col-1").hide();
 
@@ -59,7 +69,7 @@ $("#arrow-icon-1").click(function(){
  });
 
 function onOverlay() {
-    document.getElementById("overlay").style.display = "flex";
+    document.getElementById("overlay").style.display = "block";
     $("#col-3").css({"opacity": "0.2" });
     $("#col-4").css({"opacity": "0.2" });
     $("#col-5").css({"opacity": "0.2" });
@@ -73,43 +83,65 @@ function onOverlay() {
 
   }
 
-  const moveImage = window.matchMedia( "(min-width: 1020px)" );
-  const columnFour = $("#col-4");
+  // const moveImage = window.matchMedia( "(min-width: 1020px)" );
+  // const columnFour = $("#col-4");
+  // const columnFive = $("#col-5");
+  // let center, right;
+
+  // if (moveImage.matches) {
+
+  // function moveCenter() {
+
+  //     columnFour.css({"backgroundPosition": '50% 0%'},"slow");
+
+  //   }
+
+  // function moveRight() {
+
+  //   columnFour.css({"backgroundPosition": '100% 0%'},"slow");
+
+  // }
+
+  //   $("#col-4").hover(function(){
+  //     center =  setInterval(moveCenter, 3000);
+  //     right = setInterval(moveRight, 2000)
+
+  //   }, function(){
+
+  //     clearInterval(center);
+  //     clearInterval(right);
+  //     columnFour.css({"backgroundPosition": '0% 0%'},"slow");
+
+  //   });
+
+  //   function moveCenterLeft() {
+
+  //           columnFive.css({"backgroundPosition": '50% 0%'},"slow");
+
+  //          }
+
+  //   function moveLeftLeft() {
+
+  //   columnFive.css({"backgroundPosition": '0% 0%'},"slow");
+
+  //   }
+
+  //   $("#col-5").hover(function(){
+  //     centerLeft =  setInterval(moveCenterLeft, 3000);
+  //     LeftLeft = setInterval(moveLeftLeft, 2000)
+
+  //   }, function(){
+
+  //     clearInterval(centerLeft);
+  //     clearInterval(LeftLeft);
+  //     columnFive.css({"backgroundPosition": '100% 0%'},"slow");
+
+  //   });
 
 
-  if (moveImage.matches) {
-
-  function moveLeft() {
-
-      columnFour.css({"backgroundPosition": '50% 0%'},"slow");
-
-    }
-
-    $("#col-4").hover(function(){
-
-      setInterval(moveLeft, 1000);
-    }, function(){
-
-      clearInterval(moveLeft);
-
-    });
-
-  }
 
 
-//   function moveCenter() {
-
-//       columnFour.css({"backgroundPosition": '50% 0%'},"slow");
-
-//     }
-//     setInterval(moveCenter, 3000);
-
-//    function moveRight() {
-
-//       columnFour.css({"backgroundPosition": '100% 0%'},"slow");
-
-//     }
-//     setInterval(moveRight, 5000);
+  // }
 
 
 
@@ -117,18 +149,9 @@ function onOverlay() {
 
 
 
-//   } else {
-
-// alert("howdy");
-
-//   }
 
 
-//   //   columnFour.css({"backgroundPosition": '50% 0%'},"slow");
-//   //   }, function(){
-//   //     columnFour.css({"backgroundPosition": '50% 0%'},"slow");
 
-//   // });
 
 
 
