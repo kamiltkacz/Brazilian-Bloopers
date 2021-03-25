4cddf48be913262f222eac19538ce2921e8b9c08
Kamil Tkacz <kamiltkacz@gmail.com>
2021-03-24 23:16:10 -0300

    troca coluna 6 &  7, remove arrows

--- subdomain/wp-content/themes/great-idea-theme/front-page.php
+++ subdomain/wp-content/themes/great-idea-theme/front-page.php
@@ -2,7 +2,6 @@
 <?php get_header();?>
 
 
-<!-- <h1>Front Page</h1> -->
 
 <div class="container" id="homehead-cont">
 <div class="homehead">
@@ -10,30 +9,20 @@
 <div class="homehead--center--text" style="transform: rotate(2deg);">Olá</div>
 <div class="homehead--center--text" style= "transform: rotate(-2deg);"> Sejam Bem Vindos ao</div>
 <div class="homehead--center--text" style="transform: rotate(-4deg);">Ótima Ideia</div>
-
-<a href="#down-1" id="a-down-1"><i id="arrow-icon-1" class="fas fa-angle-down"></i></a>
+<div id="arrow-1-box">
+<a href="#down-1" id="a-down-1"><i id="arrow-1" class="fas fa-angle-down"></i></a>
+</div>
   </div>
  </div>
 </div>
-
-<!-- <div class="parallax">
-<div class="parallax-content">
-</div>
-</div> -->
 
  <div id="col-1" class="container">
-   <div id="arrow-div">
-  <i id="arrow-icon-2" class="fas fa-angle-left"></i>
-  </div>
  </div>
-
-
-
-
 <div id="col-2" class="container">
 <a id="down-1"></a>
 <h4 class="text_style">Somos uma equipe altamente qualificada e competente, que em apenas 7 (sete) anos produziu mais de uma centena de soluções em Marketing Digital. Temos grandes cases durante essa trajetória e parceiros satisfeitos que acreditaram em nosso conhecimento.</h4>
-<div><button id="b-sucesso" onclick="onOverlay()">Casos de Succeso&nbsp;&nbsp;<i id="arrow-icon-4" class="fas fa-angle-down"></i></button></div>
+<div id="otima-logo"></div>
+<div><button id="b-sucesso" onclick="onOverlay()">Casos de Succeso&nbsp;&nbsp;<i id="arrow-4" class="fas fa-angle-down"></i></button></div>
 </div>
 <div id="overlay" onclick="offOverlay()">
   <div class="text">Trabalhamos com as maiores marcas do Brasil</div>
@@ -41,22 +30,22 @@
 
 <div id="col-3" class="container">
 
-  <span id="hovertext-1"><a href="https://www.cvc.com.br/" class="a_brand" alt="link do CVC" target="_blank">CVC&nbsp;&nbsp;<i id="arrow-icon-5" class="fas fa-angle-right"></i></a></span>
+  <span id="hovertext-1"><a href="https://www.cvc.com.br/" class="a_brand" alt="link do CVC" target="_blank">&nbsp;&nbsp;<i id="arrow-5" class="fas fa-angle-right"></i></a></span>
 
  </div>
 
  <div id="col-4" class="container">
- <span id="hovertext-2"><a href="https://www.submarinoviagens.com.br/" class="a_brand" alt="link do Submarino" target="_blank">Submarino<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i id="arrow-icon-6" class="fas fa-angle-right"></i></a></span>
+ <span id="hovertext-2"><a href="https://www.submarinoviagens.com.br/" class="a_brand" alt="link do Submarino" target="_blank"><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i id="arrow-6" class="fas fa-angle-right"></i></a></span>
  </div>
  <div id="col-5" class="container">
- <span id="hovertext-3"><a href="https://loja.chillibeans.com.br/" class="a_brand" alt="link do Chilli Beans" target="_blank">Chilli Beans<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i id="arrow-icon-7" class="fas fa-angle-right"></i></a></span>
+ <span id="hovertext-3"><a href="https://loja.chillibeans.com.br/" class="a_brand" alt="link do Chilli Beans" target="_blank"><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i id="arrow-7" class="fas fa-angle-right"></i></a></span>
  </div>
 
 
- <div id="arrow-div-2" class="container"><a href="#down-2" id="a-down-2"><i id="arrow-icon-3" class="fas fa-angle-down"></i></a></div>
 
 
-<div id="col-7">
+
+<div id="col-6">
  <div class="container">
  <a id="down-2"></a>
   <div class="footerhead">
@@ -71,9 +60,12 @@
 </div>
 
 
-<div id="col-6" class="container">
+<div id="col-7" class="container">
  </div>
 
 
 
+
+
 <?php get_footer();?>
+
