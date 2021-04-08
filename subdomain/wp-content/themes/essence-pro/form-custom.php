ba729e449db09ea0eb96b7f4a45241531fa21908
Kamil Tkacz <kamiltkacz@gmail.com>
2021-04-08 17:32:10 -0300

    add form custom

---
+++ subdomain/wp-content/themes/essence-pro/form-custom.php
@@ -0,0 +1,23 @@
+<div class="entry-content"><div role="form" class="wpcf7" id="wpcf7-f135-p36-o1" lang="en-US" dir="ltr">
+<div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
+<form action="/contact-us/#wpcf7-f135-p36-o1" method="post" class="wpcf7-form init" novalidate="novalidate" data-status="init">
+<div style="display: none;">
+<input type="hidden" name="_wpcf7" value="135" />
+<input type="hidden" name="_wpcf7_version" value="5.4" />
+<input type="hidden" name="_wpcf7_locale" value="en_US" />
+<input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f135-p36-o1" />
+<input type="hidden" name="_wpcf7_container_post" value="36" />
+<input type="hidden" name="_wpcf7_posted_data_hash" value="" />
+</div>
+<p>Your Name (required)<br />
+    <span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" /></span> </p>
+<p>Your Phone (required)<br />
+    <span class="wpcf7-form-control-wrap your-phone"><input type="text" name="your-phone" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" /></span> </p>
+<p>Your Email (required)<br />
+    <span class="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" /></span> </p>
+<p>Type of Event<br />
+<span class="wpcf7-form-control-wrap radio-549"><span class="wpcf7-form-control wpcf7-radio"><span class="wpcf7-list-item first"><input type="radio" name="radio-549" value="Wedding" /><span class="wpcf7-list-item-label">Wedding</span></span><span class="wpcf7-list-item"><input type="radio" name="radio-549" value="Bar / Bat Mitzvah" /><span class="wpcf7-list-item-label">Bar / Bat Mitzvah</span></span><span class="wpcf7-list-item"><input type="radio" name="radio-549" value="Corporate Retreat" /><span class="wpcf7-list-item-label">Corporate Retreat</span></span><span class="wpcf7-list-item last"><input type="radio" name="radio-549" value="Other (Describe below)" /><span class="wpcf7-list-item-label">Other (Describe below)</span></span></span></span></p>
+<p>Describe Your Event / Comments<br />
+    <span class="wpcf7-form-control-wrap your-event"><textarea name="your-event" cols="96" rows="8" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false"></textarea></span> </p>
+<p><input type="submit" value="Send" class="wpcf7-form-control wpcf7-submit" /></p>
+<div class="wpcf7-response-output" aria-hidden="true"></div></form></div>
