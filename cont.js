535440d216c64793de063309cb15150c408f0abb
Kamil Tkacz <kamiltkacz@gmail.com>
2021-04-20 17:38:42 -0300

    logo bind

--- cont.js
+++ cont.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function () {
     function sucesso() {
-      $("#home").show("slow");
+      $("#home").show("slow").css({display: "flex"});
+      $("#arrow-1").show("slow");
 
     }
     setTimeout(sucesso, 2000);
@@ -14,3 +15,28 @@ $(document).ready(function () {
     }
     setTimeout(sucesso, 3000);
   });
+
+//   $(document).bind('mousemove', function(e){
+//     var width = $('#logo_main').width();
+//     $('#logo_main').css({
+//        left:  e.pageX-width,
+//        top:   e.pageY
+//     });
+
+// });
+
+// $(document).click(function(){
+
+//   $(this).unbind('mousemove');
+//   $('#logo_main').css({
+//     left:  "",
+//     top:   ""
+//  });
+
+
+//  });
+
+
+
+
+
